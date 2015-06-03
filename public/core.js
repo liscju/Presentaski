b2a6a26b64b5f71ac1815c5be75aaabdda962aa6
liscju <piotr.listkiewicz@gmail.com>
2015-06-03 13:43:50 +0200

    Added option to draw from toolbox pen

--- public/core.js
+++ public/core.js
@@ -2,5 +2,39 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainPresentationController',function ($scope, $http) {
-    $scope.greeting = "Witaj uzytkowniku"
+    $scope.cursor_type = "cursor";
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.pageX - rect.left,
+            y: evt.pageY - rect.top
+        };
+    }
+
+    $scope.init = function () {
+        var drawing_area_canvas = document.querySelector( '#drawing_area' );
+        $scope.drawing_area_canvas_ctx = drawing_area_canvas.getContext("2d");
+        drawing_area_canvas.addEventListener('mousemove', function (evt) {
+                var mouse = getMousePos(drawing_area_canvas, evt);
+                if (evt.which == 1)
+                    $scope.canvasClicked(mouse);
+        });
+    };
+
+    $scope.chooseCursor = function () {
+        $scope.cursor_type = "cursor";
+    };
+
+    $scope.choosePen = function () {
+        $scope.cursor_type = "pen";
+    };
+
+    $scope.canvasClicked = function(pos) {
+        if ($scope.cursor_type == "pen") {
+            $scope.drawing_area_canvas_ctx.beginPath();
+            $scope.drawing_area_canvas_ctx.arc(pos.x,pos.y,0.1,0,2*Math.PI);
+            $scope.drawing_area_canvas_ctx.stroke();
+        }
+    };
 });
